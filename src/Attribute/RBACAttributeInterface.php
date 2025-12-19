c1b26de233ce4ae15bf242af4e435645e5d6dd8a
kor3k <martin.koranda@gmail.com>
2025-12-19 21:26:51 +0100

    remove #[\Attribute] from RBACAttributeInterface
    php 8.5 fails with attributes on interfaces
    https://www.php.net/manual/en/migration85.incompatible.php

--- src/Attribute/RBACAttributeInterface.php
+++ src/Attribute/RBACAttributeInterface.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace PhpRbacBundle\Attribute;
 
-use Attribute;
 use PhpRbacBundle\Core\RbacInterface;
 
-#[Attribute]
 interface RBACAttributeInterface
 {
     public function getSecurityCheckMethod(RbacInterface $accessControl, mixed $userId): bool;
